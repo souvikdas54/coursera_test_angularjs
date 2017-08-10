a052dec60036a206c436903b20bd71c3e812f882
souvikdas54 <85souvikdas@gmail.com>
2017-08-10 23:08:35 +0530

    Third Assignment

---
+++ Angularassignment3/app.js
@@ -0,0 +1,82 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+ .directive('foundItems', foundItemsDirective)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json");
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var narrowCtrl = this;
+  narrowCtrl.menuItem = "";
+  narrowCtrl.found = [];
+  narrowCtrl.nothingFoundFlag = false;
+
+  narrowCtrl.getMatchedItems = function () {
+    if (narrowCtrl.menuItem == "") {
+       narrowCtrl.nothingFoundFlag = true;
+    } else {
+
+    var promise = MenuSearchService.getMatchedMenuItems(narrowCtrl.menuItem);
+    promise.then(function (result) {
+      narrowCtrl.found = MenuSearchService.getFoundItems(result,narrowCtrl.menuItem);
+      if (narrowCtrl.found.length != 0) {
+        narrowCtrl.nothingFoundFlag = false;
+      } else {
+         narrowCtrl.nothingFoundFlag = true;
+      }
+     }, function (errorResponse) {
+       console.log(errorResponse.message);
+     });
+   }
+  }
+
+  narrowCtrl.removeItem = function (itemIndex) {
+    narrowCtrl.found.splice(itemIndex, 1);
+  };
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http,ApiBasePath) {
+  var service = this;
+
+service.getFoundItems = function (itemsList,searchTerm) {
+  var foundItems = [];
+      for (var i=0; i < itemsList.data.menu_items.length; i++) {
+        if (itemsList.data.menu_items[i].description.indexOf(searchTerm) !== -1) {
+          foundItems.push(itemsList.data.menu_items[i]);
+        }
+      }
+      return foundItems;
+}
+  service.getMatchedMenuItems = function (searchTerm) {
+     var jsonItems = $http({
+      method: "GET",
+      url: ApiBasePath
+    });
+  return jsonItems;
+  };
+}
+
+function foundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+    controller: foundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+ function foundItemsDirectiveController() {
+ }
+
+
+})();
